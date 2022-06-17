28231b1a738e5b9fc00f4c3c9a6f875190658825
Shai Yehezkel <shaiyehezkel@campus.technion.ac.il>
2022-06-17 17:45:37 +0300

    Added bonus

--- HW3/MDP/bonus.py
+++ HW3/MDP/bonus.py
@@ -1,9 +1,11 @@
 """ You can import what ever you want """
-
+import pygame
+from copy import deepcopy
+from value_and_policy_iteration import _evaluate_state_iteration_
 
 def get_all_policies(mdp, U):  # You can add more input parameters as needed
     # TODO:
-    # Given the mdp, and the utility value U (which satisfies the Belman equation)
+    # Given the mdp, and the utility value U (which satisfies the Belmann equation)
     # print / display all the policies that maintain this value
     # (a visualization must be performed to display all the policies)
     #
@@ -11,7 +13,17 @@ def get_all_policies(mdp, U):  # You can add more input parameters as needed
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    policy = deepcopy(U)
+    for i in range(len(mdp.board)):
+        for j in range(len(mdp.board[i])):
+            S = (i, j)
+            max_sum, max_actions, Reward = _evaluate_state_iteration_(mdp, U, S)
+            if max_sum is None:
+                continue
+            policy[i][j] = max_actions
+
+
+    return policy
     # ========================
 
 
