cc8f4ff05215742e66038d2d004dc2ecbaae8eb3
DorinRu <103881146+DorinRu@users.noreply.github.com>
2022-06-17 17:11:46 +0300

    HW3 - section1

--- HW3/MDP/value_and_policy_iteration.py
+++ HW3/MDP/value_and_policy_iteration.py
@@ -1,7 +1,7 @@
 import math
 from copy import deepcopy
 import numpy as np
-
+actions_mapping = ['UP', 'DOWN', 'RIGHT', 'LEFT']
 
 def is_float(st):
     try:
@@ -9,30 +9,40 @@ def is_float(st):
     except ValueError:
         return None
 
+def _calc_prob_sum_(mdp, U, action_wanted, S):
+
+    prob_sum = 0
+    for i_action, action_prob in enumerate(mdp.transition_function[action_wanted]):
+        new_state = mdp.step(S, actions_mapping[i_action])
+        prob_sum += action_prob * U[new_state[0]][new_state[1]]
+    return prob_sum
+
+def _evaluate_state_iteration_(mdp, U, S):
+
+    Reward = is_float(mdp.board[S[0]][S[1]])  # Reward for each cell R(s)
+    if Reward is None:
+        return None, None, None
+    max_action = actions_mapping[0]
+    max_sum = 0
+    if S not in mdp.terminal_states:
+        for action_wanted in mdp.actions.keys():
+            prob_sum = _calc_prob_sum_(mdp, U, action_wanted, S)
+            if prob_sum > max_sum:
+                max_sum = prob_sum
+                max_action = action_wanted
+    return max_sum, max_action, Reward
 
 def _value_interation_policy(mdp, U):
-    actions_mapping = ['UP', 'DOWN', 'RIGHT', 'LEFT']
+
     delta = 0
     U_temp = deepcopy(U)
     policy = deepcopy(U)
     for i in range(len(mdp.board)):
         for j in range(len(mdp.board[i])):
-            Reward = is_float(mdp.board[i][j])  # Reward for each cell R(s)
-            if Reward is None:
+            S = (i,j)
+            max_sum , max_action, Reward = _evaluate_state_iteration_(mdp, U, S)
+            if max_sum is None:
                 continue
-            max_action = actions_mapping[0]
-            max_sum = 0
-            if (i, j) not in mdp.terminal_states:
-                for action_wanted in mdp.actions.keys():
-                    prob_sum = 0
-                    for i_action, action_prob in enumerate(mdp.transition_function[action_wanted]):
-                        new_state = mdp.step((i, j), actions_mapping[i_action])
-                        prob_sum += action_prob * U[new_state[0]][new_state[1]]
-
-                    if prob_sum > max_sum:
-                        max_sum = prob_sum
-                        max_action = action_wanted
-
             policy[i][j] = max_action
             U_temp[i][j] = Reward + mdp.gamma * max_sum
             delta = max(delta, abs(U_temp[i][j] - U[i][j]))
@@ -79,7 +89,33 @@ def policy_evaluation(mdp, policy):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    actions_mapping = ['UP', 'DOWN', 'RIGHT', 'LEFT']
+    n = mdp.num_col*mdp.num_row
+    I = np.eye(n)
+    P = np.zeros((n,n))
+    R = np.zeros((n,1))
+    for i in range(len(mdp.board)):
+        for j in range(len(mdp.board[i])):
+            Reward = is_float(mdp.board[i][j])  # Reward for each cell R(s)
+            if Reward is None:
+                continue
+            R[i + j * mdp.num_row] = Reward
+            if (i, j) in mdp.terminal_states:
+                continue
+            prob_vec = np.zeros((1, n))
+            chosen_action = policy[i][j]
+            for i_action, action_prob in enumerate(mdp.transition_function[chosen_action]):
+                new_state = mdp.step((i, j), actions_mapping[i_action])
+                col_index = new_state[0]+new_state[1]*mdp.num_row
+                prob_vec[0,col_index] += action_prob
+
+
+            P[i+j*mdp.num_row,:]=prob_vec
+
+    U = np.linalg.inv(I-mdp.gamma*P)@R
+    U = U.reshape((mdp.num_col,mdp.num_row)).T.tolist()
+    return U
+
     # ========================
 
 
@@ -91,5 +127,21 @@ def policy_iteration(mdp, policy_init):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    unchanged = False
+    policy = deepcopy(policy_init)
+    while not unchanged:
+        U = policy_evaluation(mdp, policy_init)
+        unchanged = True
+        for i in range(len(mdp.board)):
+            for j in range(len(mdp.board[i])):
+                S = (i,j)
+                max_sum , max_action, _ = _evaluate_state_iteration_(mdp, U, S)
+                if max_sum is None or S in mdp.terminal_states:
+                    continue
+                action_wanted = policy[i][j]
+                current_sum = _calc_prob_sum_(mdp, U, action_wanted, S)
+                if max_sum > current_sum:
+                    policy[i][j] = max_action
+                    unchanged =False
+    return policy
     # ========================
